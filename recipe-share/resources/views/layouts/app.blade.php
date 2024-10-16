5bfe9808395c090fd0109fe0c77ea52db23e1c67
rifie536 <maitakaz536@gmail.com>
2024-10-16 20:56:17 +0900

    homeビューを作成

--- recipe-share/resources/views/layouts/app.blade.php
+++ recipe-share/resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
             @endif
 
             <!-- Page Content -->
-            <main>
+            <main class="container mx-auto pt-8">
                 {{ $slot }}
             </main>
         </div>
